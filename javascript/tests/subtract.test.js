e1893b120ca0623b58798c82a3951ae4211162fa
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-12-29 19:57:10 -0300

    Cambio de titulo al test de resta

--- javascript/tests/subtract.test.js
+++ javascript/tests/subtract.test.js
@@ -1,6 +1,6 @@
 const subtract = require('../funciones/subtract')
 
-describe("Resta de dos numeros", () => {
+describe("subtract", () => {
     it('La resta de 3 - 1 deberia dar 2', () => {
         expect(subtract(3,1)).toBe(2)
     })
